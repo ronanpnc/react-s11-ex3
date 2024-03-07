436e76305d5c828ef771b0eb4d11ba1400fa04e3
ronan.ogor <ronan.ogor@passerellesnumeriques.org>
2024-03-08 01:15:10 +0700

    first release

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import React from "react";
+
+import OrderCard from "./components/OrderCard";
+import CheckoutButton from "./components/CheckoutButton";
+
+const ORDERS = [
+  {
+    product: "Banana",
+    price: 54.6,
+    quantity: 3,
+  },
+  {
+    product: "Computer",
+    price: 100.5,
+    quantity: 4,
+  },
+  {
+    product: "Table",
+    price: 1070,
+    quantity: 3,
+  },
+];
+
+export default function App() {
+  const [orders, setOrders] = React.useState(ORDERS);
+
+  const getTotal = () => {
+    let result = 0;
+    orders.map((order) => {
+      result += order.quantity * order.price;
+    });
+
+    return result;
+  };
+
+  const handeQuantityChanged = (product, newQuantity) => {
+    setOrders(
+      orders.map((order) => {
+        if (order.product === product) {
+          return { ...order, quantity: newQuantity };
+        }
+        return order;
+      })
+    );
+  };
+
+  return (
+    <>
+      <header>
+        <h1>Your orders</h1>
+      </header>
+
+      <div className="order-list">
+        {orders.map((order, index) => (
+          <OrderCard
+            key={index}
+            product={order.product}
+            productPrice={order.price}
+            quantity={order.quantity}
+            onQuantityChanged={handeQuantityChanged}
+          />
+        ))}
+      </div>
+
+      <CheckoutButton total={getTotal()}></CheckoutButton>
+    </>
+  );
+}
---
+++ src/components/OrderCard.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+export default function OrderCard({
+  product,
+  productPrice,
+  quantity,
+  onQuantityChanged,
+}) {
+  const format = (price) => "$ " + price;
+
+  const removeQuantity = () => {
+    if (canRemoveQuantity()) {
+      console.log("dd");
+      onQuantityChanged(product, quantity - 1);
+    }
+  };
+  const addQuantity = () => {
+    onQuantityChanged(product, quantity + 1);
+  };
+
+  const removeQuantityButtonStyle = () => {
+    return canRemoveQuantity() ? undefined : { backgroundColor: "#bfbfbf" };
+  };
+
+  const canRemoveQuantity = () => {
+    return quantity > 0;
+  };
+
+  return (
+    <div className="order">
+      <div>
+        <h4>{product}</h4>
+        <small>{format(productPrice)}</small>
+      </div>
+
+      <div className="order-quantity">
+        <div
+          className="order-button"
+          style={removeQuantityButtonStyle()}
+          onClick={removeQuantity}
+        >
+          -
+        </div>
+        <h4>{quantity}</h4>
+        <div className="order-button" onClick={addQuantity}>
+          +
+        </div>
+      </div>
+    </div>
+  );
+}
